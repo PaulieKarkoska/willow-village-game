6776cc9994b2dde9bf766b9bdaee36717bf2bed4
Paul Karkoska <paul.karkoska@gmail.com>
2020-08-29 14:05:53 -0500

    Added main menu and preferences manager

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -1,18 +1,81 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Panels")]
+    public GameObject menuPanel;
+    public GameObject newGamePanel;
+    public GameObject settingsPanel;
+
+    [Header("Menu Buttons")]
+    public Button NewGameButton;
+    public Button ContinueButton;
+    public Button SettingsButton;
+    public Button ExitButton;
+
+    [Header("New Game Buttons")]
+    public Button NewGame_YesButton;
+    public Button NewGame_NoButton;
+
+    private void Start()
     {
-        
+        newGamePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+
+        //if (SaveManager.HasSave)
+        // Disable Continue Button
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewGame()
     {
-        
+        //if (SavaManager.HasSave)
+        if (true)
+        {
+            TogglePanel(newGamePanel, true);
+        }
+        else
+        {
+            //SaveManager.ClearCurrentSave();
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void NewGameYes()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void NewGameNo()
+    {
+        TogglePanel(newGamePanel, false);
+    }
+
+    public void Continue()
+    {
+        Debug.Log("continue pressed");
+
+    }
+
+    public void Settings()
+    {
+        Debug.Log("settings pressed");
+
+    }
+
+    public void Exit()
+    {
+        Debug.Log("exit pressed");
+
+    }
+
+    private void TogglePanel(GameObject panel, bool active)
+    {
+        panel.SetActive(active);
+        NewGameButton.interactable = !active;
+        ContinueButton.interactable = !active;
+        SettingsButton.interactable = !active;
+        ExitButton.interactable = !active;
     }
 }
---
+++ Assets/Scripts/Preferences/Preferences.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public static class Preferences
+{
+    private const string MUSIC_VOL_KEY = "MusicVol";
+    private const string SFX_VOL_KEY = "SfxVol";
+
+    static Preferences()
+    {
+        if (!PlayerPrefs.HasKey(MUSIC_VOL_KEY))
+            PlayerPrefs.SetFloat(MUSIC_VOL_KEY, 1);
+
+        if (!PlayerPrefs.HasKey(SFX_VOL_KEY))
+            PlayerPrefs.SetFloat(SFX_VOL_KEY, 1);
+    }
+
+    public static float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat(MUSIC_VOL_KEY);
+    }
+    public static void SetMusicVolume(float volume)
+    {
+        PlayerPrefs.SetFloat(MUSIC_VOL_KEY, volume);
+    }
+
+    public static float GetSfxVolume()
+    {
+        return PlayerPrefs.GetFloat(SFX_VOL_KEY);
+    }
+    public static void SetSfxVolume(float volume)
+    {
+        PlayerPrefs.SetFloat(SFX_VOL_KEY, volume);
+    }
+}
+
+
