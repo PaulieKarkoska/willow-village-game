945370f7d5da6dba48f1edb2742bf9a2d154a8d8
Paul Karkoska <paul.karkoska@gmail.com>
2020-09-15 20:06:29 -0500

    Weapon pickup/drop
    Added weapon pickup/drop mechanic. Still need to fix shield pickup/drop

--- Assets/Scripts/Equipment/EquipmentHelper.cs
+++ Assets/Scripts/Equipment/EquipmentHelper.cs
@@ -1,7 +1,6 @@
 ﻿using Invector.vItemManager;
 using Invector.vMelee;
 using UnityEngine;
-using UnityEngine.Internal.VR;
 
 public class EquipmentHelper : MonoBehaviour
 {
@@ -12,71 +11,99 @@ public class EquipmentHelper : MonoBehaviour
 
     private vItem _currentWeapon;
     private vItem _lastWeapon;
+    private EquipPoint _weaponEquipPoint;
 
     private vItem _currentShield;
     private vItem _lastShield;
+    private EquipPoint _shieldEquipPoint;
 
-    //private void Start()
+    private void Start()
+    {
+        _shieldEquipPoint = _itemManager.equipPoints[0];
+        _weaponEquipPoint = _itemManager.equipPoints[1];
+    }
+
+    //public void AddItem(vItem item)
+    //{
+    //    if (item == null) return;
+
+    //    switch (item.type)
+    //    {
+    //        case vItemType.MeleeWeapon:
+    //            //_itemManager.AutoEquipItem(item, 0, true, true);
+    //            //DropOldWeapon(item);
+    //            break;
+
+    //        case vItemType.Defense:
+    //            //DropOldShield(item);
+    //            //_itemManager.AutoEquipItem(item, 2, true, true);
+    //            break;
+
+    //        case vItemType.Money:
+    //            break;
+    //    }
+    //}
+
+    //private void DropOldWeapon(vItem newWeapon)
+    //{
+    //    _lastWeapon = _currentWeapon;
+    //    _currentWeapon = newWeapon;
+    //    //_meleeManager.SetRightWeapon(newWeapon.originalObject);
+    //    if (_lastWeapon)
+    //        _itemManager.DestroyCurrentEquipedItem(0);
+
+    //    Debug.Log(_meleeManager.rightWeapon?.ToString());
+    //}
+
+    //private void DropOldShield(vItem newShield)
+    //{
+    //    _lastShield = _currentShield;
+    //    _currentShield = newShield;
+    //    //_meleeManager.SetLeftWeapon(newShield.originalObject);
+    //    if (_lastShield)
+    //        _itemManager.DestroyCurrentEquipedItem(1);
+
+    //    Debug.Log(_meleeManager.leftWeapon?.ToString());
+    //}
+
+
+    public void DropItemOnUnequip(vEquipArea area, vItem item)
+    {
+        Transform handle;
+        switch (item.type)
+        {
+            case vItemType.MeleeWeapon:
+                handle = _weaponEquipPoint.handler.defaultHandler;
+                //_itemManager.items.Remove(item);
+                Instantiate(item.dropObject, handle.position + (handle.forward * 0.15f), handle.rotation);
+                break;
+
+            case vItemType.Defense:
+                handle = _shieldEquipPoint.handler.defaultHandler;
+                //_itemManager.items.Remove(item);
+                Instantiate(item.dropObject, handle.position + (handle.forward * 0.15f), handle.rotation);
+                break;
+        }
+    }
+
+    public void DestroyItemOnFinishUnequip(vEquipArea area, vItem item)
+    {
+        switch (item.type)
+        {
+            case vItemType.MeleeWeapon:
+            case vItemType.Defense:
+                _itemManager.DestroyItem(item);
+                break;
+        }
+    }
+
+    //public void EquipItem(vEquipArea area, vItem item)
     //{
 
     //}
 
-    public void AddItem(vItem item)
-    {
-        if (item == null) return;
+    //public void UnequipItem(vEquipArea area, vItem item)
+    //{
 
-        switch (item.type)
-        {
-            case vItemType.MeleeWeapon:
-                _itemManager.AutoEquipItem(item, 0, true, false);
-                break;
-
-            case vItemType.Defense:
-                SwapShields(item);
-                break;
-
-            case vItemType.Money:
-                break;
-        }
-    }
-
-    private void SwapWeapons(vItem newWeapon)
-    {
-        _currentWeapon = newWeapon;
-        _meleeManager.SetRightWeapon(newWeapon.originalObject);
-        Debug.Log(_meleeManager.rightWeapon?.ToString());
-    }
-
-    private void SwapShields(vItem newShield)
-    {
-        _currentShield = newShield;
-        _meleeManager.SetLeftWeapon(newShield.originalObject);
-        Debug.Log(_meleeManager.leftWeapon?.ToString());
-    }
-
-
-
-    public void DropItem(vItem item, int count)
-    {
-        switch (item.type)
-        {
-            case vItemType.MeleeWeapon:
-                _lastWeapon = item;
-                break;
-
-            case vItemType.Defense:
-                _lastShield = item;
-                break;
-        }
-    }
-
-    public void EquipItem(vEquipArea area, vItem item)
-    {
-
-    }
-
-    public void UnequipItem(vEquipArea area, vItem item)
-    {
-
-    }
+    //}
 }
