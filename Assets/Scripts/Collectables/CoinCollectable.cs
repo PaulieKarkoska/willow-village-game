bcf18d4c95e0d5872687cf7092facab2215a2664
Paul Karkoska <paul.karkoska@gmail.com>
2020-09-21 22:36:06 -0500

    Added money system

---
+++ Assets/Scripts/Collectables/CoinCollectable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollectable : MonoBehaviour
+{
+    public int value = 1;
+    [SerializeField]
+    private GameObject _effectPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var mgr = other.GetComponent<MoneyManager>();
+            mgr.AddMoney(value);
+            Instantiate(_effectPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Collectables/MoneyManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public int totalMoney;
+
+    public int AddMoney(int money)
+    {
+        totalMoney += money;
+        return totalMoney;
+    }
+
+    public int TakeMoney(int money)
+    {
+        totalMoney -= money;
+        return totalMoney;
+    }
+
+    public bool HasEnough(int cost)
+    {
+        return cost <= totalMoney;
+    }
+
+}
