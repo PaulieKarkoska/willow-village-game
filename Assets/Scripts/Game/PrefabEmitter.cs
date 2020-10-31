addecf37e611041935e109704370cb094cd766c7
Paulie Karkoska <31753628+PaulieKarkoska@users.noreply.github.com>
2020-10-31 17:15:44 -0500

    Updated Enemy AI and created enemy spawn area

--- Assets/Scripts/Game/PrefabEmitter.cs
+++ Assets/Scripts/Game/PrefabEmitter.cs
@@ -8,20 +8,47 @@ public class PrefabEmitter : MonoBehaviour
     [SerializeField]
     private GameObject[] prefabs;
 
+    [Header("Emission")]
+    [SerializeField]
+    private bool _emitOnStart = false;
+    [SerializeField]
+    private float _startEmissionDelay = 0;
+    [SerializeField]
+    private Vector3 _positionJitter = Vector3.zero;
 
-    //private void Start()
-    //{
-    //    StartCoroutine(Test());
-    //}
+    [Header("Continuous")]
+    [SerializeField]
+    private bool _isContinuous = false;
+    [SerializeField]
+    private float _continuousEmissionsPerSecond = 1;
+    [SerializeField]
+    private float _continuousEmissionJitter = 0;
 
-    //private IEnumerator Test()
-    //{
-    //    while (true)
-    //    {
-    //        yield return new WaitForSeconds(3);
-    //        Emit(transform.position, Vector3.up * 150, new Vector3(0.1f, 0.1f, 0.1f), 0, 5, 10);
-    //    }
-    //}
+    private void Start()
+    {
+        if (_emitOnStart)
+            StartCoroutine(EmitOnStart());
+
+        if (_isContinuous)
+            StartCoroutine(EmitContinuously());
+    }
+
+    private IEnumerator EmitOnStart()
+    {
+        yield return new WaitForSeconds(_startEmissionDelay);
+        Emit(transform.position, Vector3.zero, _positionJitter, 0, 1, 1);
+    }
+
+    private IEnumerator EmitContinuously()
+    {
+        while (_isContinuous)
+        {
+            var rate = 1 * _continuousEmissionsPerSecond;
+            rate += Random.Range(rate - _continuousEmissionJitter, rate + _continuousEmissionJitter);
+            yield return new WaitForSeconds(rate);
+            Emit(transform.position, Vector3.zero, _positionJitter, 0, 1, 1);
+        }
+    }
 
     public GameObject[] Emit(Vector3 startPos, Vector3 force, Vector3 jitter, int prefabIndex, int emissionMinCount, int emissionMaxCount)
     {
